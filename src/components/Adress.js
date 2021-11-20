f7e4f66484f7cef2c1bd84bd888b13b1ede42fe4
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-11-20 18:05:19 -0300

    adress component layout added

--- src/components/Adress.js
+++ src/components/Adress.js
@@ -2,39 +2,91 @@ import styled from 'styled-components';
 import imgAdress from '.././assets/image03.jpg'
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
-export default function Details() {
+export default function Adress() {
     return (
         <Container>
             <h1>Bom te ver por aqui, @User.</h1>
             <h2>"Agradecer é arte de atrair coisas boas"</h2>
-            <Options>
+            <AdressInputs>
                 <img src={imgAdress} alt="pessoa meditando"/>
-                <PlanType>
-                    <h3>Plano: </h3>
-                    <MdKeyboardArrowDownStyled/>
-                </PlanType>
-
-                <Delivery>
-                    <h3>Entrega </h3>
-                    <MdKeyboardArrowDownStyled/>
-                </Delivery> 
-
-                <Products>
-                    <h3> Quero receber </h3>
-                    <h4> Chás </h4>
-                    <h4> Produtos orgânicos </h4>
-                    <h4> Incesos </h4>
-                </Products>
-            </Options>
-            <button>Próximo</button>
+                <input type="text" placeholder="Nome completo"/>
+                <input type="text" placeholder="Endereço de entrega"/>
+                <input type="text" placeholder="CEP"/>                    
+                <input type="text" placeholder="Cidade"/>
+                <button>
+                    <h3>Estado</h3> <MdKeyboardArrowDownStyled/>                            
+                </button>                           
+            </AdressInputs>
+            <Button>Finalizar</Button>
         </Container>
     )
 }
 
+const AdressInputs = styled.div`
+    width: 356px;
+    height: 429px;
+    background: pink;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Roboto;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 21px;
+    background-color: #ffffff;
+
+    img{
+        width: 300px;
+        height: 172px;
+        border-radius: 25px;
+    }
+
+    input{
+        display: flex;
+        justify-content: space-between;
+        padding-left: 10px;
+        width: 290px;
+        height: 44px;
+        border-radius: 5px;
+        background-color: #E0D1ED;
+        border-color: #E0D1ED;
+        margin-bottom: 7px;
+        text-align: start;
+        font-family: Roboto;
+        font-weight: bold;
+        font-size: 18px;
+        line-height: 21px;
+        color: #4D65A8;
+        ::placeholder,
+        ::-webkit-input-placeholder {     
+            color: #4D65A8;
+        }
+    }
+    button{
+        display: flex;
+        justify-content: space-between;
+        padding: 0px 10px 0px 10px;
+        width: 290px;
+        height: 44px;
+        background-color: #E0D1ED;
+        color: #4D65A8;
+        border-radius: 5px;
+        font-family: Roboto;
+        font-weight: bold;
+        font-size: 18px;
+        line-height: 21px;
+    } 
+    h3{
+        padding-top: 10px;
+    }
+    
+`;
+
 const MdKeyboardArrowDownStyled = styled(MdKeyboardArrowDown)`
     color: #4D65A8;
     font-size: 35px;
-    height: 34px;
+    height: 44px;
 `;
 
 const Container = styled.div`
@@ -44,7 +96,7 @@ const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
+    
     h1{
         width: 341px;
         height: 29px;
@@ -61,104 +113,27 @@ const Container = styled.div`
         line-height: 21px;
         margin: 0 auto;
     }
-    button{     
-        width: 202px;
-        height: 39px;
-        background: #8C97EA;
-        border-color: #8C97EA;
-        border-radius: 10px;
-        margin: 31px auto 23px auto;
-        font-weight: 500;
-        font-size: 20px;
-        line-height: 28px;
-        color: #FFFFFF;
-        font-family: Roboto;
-        text-align: center;
-    }
 `;
 
-const Options = styled.div`
-    width: 356px;
-    height: 429px;
-    background: pink;
+const Button = styled.button `     
+    width: 202px;
+    height: 39px;
+    background: #8C97EA;
+    border-color: #8C97EA;
     border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    margin: 31px auto 23px auto;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 28px;
+    color: #FFFFFF;
     font-family: Roboto;
-    font-weight: bold;
-    font-size: 18px;
-    line-height: 21px;
-
-    background-color: #ffffff;
-
-    img{
-        width: 300px;
-        height: 172px;
-        border-radius: 25px;
-    }
-`;
-const PlanType = styled.div`  
-    display: flex;
-    justify-content: space-between;
-    padding: 10px 10px 0 10px;
-    width: 290px;
-    height: 44px;
-    border-radius: 5px;
-    background-color: #E0D1ED;
-    margin-bottom: 7px;
-    h3{
-        color: #4D65A8;
-    }  
+    text-align: center;
 `;
 
-const Delivery = styled.div`  
-    display: flex; 
-    justify-content: space-between;
-    padding: 10px 10px 0 10px;
-    width: 290px;
-    height: 44px;
-    border-radius: 5px;
-    background-color: #E0D1ED;
-    margin-bottom: 7px;
-    h3{
-        color: #4D65A8;        
-    }
-    h4{
-        color: #E63C80;
-    }   
-`;
 
-const Products = styled.div`  
-    display: flex; 
-    justify-content: space-between;
-    padding: 10px 10px 0 10px;
-    width: 290px;
-    height: 111px;
-    border-radius: 5px;
-    background-color: #E0D1ED;
-    margin-bottom: 7px;
-    h3{
-        color: #4D65A8;        
-    }
-    h4{    
-        width: 270px;
-        text-align: end;
-        color: #E63C80;
-        margin-bottom: 10px;
-    }   
-`;
 
-const PlanProducts = styled.div`  
-    display: flex; 
-    justify-content: space-between;
-    padding: 10px 10px 0 10px;
-    width: 340px;
-    h5{
-        font-weight: normal;
-        font-size: 18px;
-        line-height: 21px;
-        color: #E63C80;     
-    }      
-`;
+
+
+
+
 
